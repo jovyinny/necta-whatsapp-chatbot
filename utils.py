c5bbbe441cc1a2c9ea096d7e5243a96633c26ff2
jovyinny <jovinemutelani@gmail.com>
2023-09-14 10:29:48 +0300

    modified_files = ['.gitignore', 'utils.py'] new files=['Imgs/Performance/image_ids.json'] Reason: Added image_ids.json to the repo as it contains images id of images uploaded to whatsapp

--- utils.py
+++ utils.py
@@ -1,25 +1,20 @@
 import os
 import pytz
 import json
+import math
 import logging
-from nectaapi import student,summary,comparison
 import pandas as pd
-import matplotlib.pyplot as plt
 from datetime import datetime
-import pymongo
-from urllib.parse import quote_plus
-
-Mongo_client = pymongo.MongoClient(os.getenv("MONGODB_URI"))
-
-MY_DB=Mongo_client["NectaAPI"]
-performance_collection=MY_DB["Performance"]
-
+import matplotlib.pyplot as plt
+from PIL import Image,ImageFilter
+from nectaapi import student,summary,comparison
 
 
 # Logging
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
+
 
 async def structure_student_results_message(data):
   year=data.get("matokeo_year")
@@ -28,23 +23,19 @@ async def structure_student_results_message(data):
   student_number=(data.get("matokeo_student_number"))
   
   try:
-    
     results = student.student(year, exam_type, school_number, student_number)
-    
     if results:
       subjects=[f"{sub} >> {score}" for sub,score in results.get("subjects").items()]
       
       message=f"""Matokeo:\nschool name:{results.get('school_name')}\ngender:{results.get('gender')}\nDivision:{results.get('division')}\nPoints:{results.get('points')}\nsubjects score: {subjects}
       """
+      logging.info(f"Done structuring results message of {student_number} from {school_number}")
       return message
 
   except Exception as error:
     logging.error(f"{error} while structuring student results summary")
     return "Samahani sijaweza kupata matokeo yako"
 
-
-
-# comparison.schoolPresent(year, exam_type, school_number)
 
 async def school_summary(data:dict,messenger):
   year=data.get("ufaulu_wa_shule_year")
@@ -53,8 +44,8 @@ async def school_summary(data:dict,messenger):
   image_name=f"{year}_{exam_type}_{str(school_number).upper()}.png"
   
   if os.path.exists(os.path.join("./Imgs/Performance",image_name)):
-    print("here")
-    return get_saved_details(image_name,messenger)
+    ids_file_path='./Imgs/Performance/image_ids.json'
+    return get_saved_details(image_name,ids_file_path,messenger)
   
   try:
     # check if the school participated in exams in particular year
@@ -68,28 +59,30 @@ async def school_summary(data:dict,messenger):
       if not data.empty:
         plot_columns=["school_name","year_of_exam","division_one","division_two","division_three","division_four","division_zero","male_students","female_students","absentees"]
         plot_data=data[plot_columns]
-        file=create_plots(plot_data,image_name)
+        file_path=create_performance_plots(plot_data,image_name)
         
-        if file is not None:
+        if file_path is not None:
           school_name=data.school_name.values[0].replace("'","")
           region=data.school_region.values[0].replace("'","")
           gpa=data.gpa.values[0].replace("'","")
           
           message=f"""School Name: {school_name}\nRegion: {region}\nGPA: *{gpa}*"""
-          response=save_details({
+          ids_file_path='./Imgs/Performance/image_ids.json'
+          
+          response=await save_details({
             "name":image_name,
-            "file_path":file,
+            "file_path":file_path,
             "message":message,
             "caption": f"Ufaulu wa shule *{school_name}*"
-          },messenger)
+          },ids_file_path,messenger)
+
+          logging.info("Done obtaining school summary")
           
           return response
-  
       return None
   except  Exception as error:
     logging.error(f"{error} while getting summary")
     return None
-  pass
 
 
 def autopct_format(values):
@@ -99,8 +92,7 @@ def autopct_format(values):
             return '{:.1f}%\n({num:d})'.format(pct, num=val)
         return custom_format
   
-
-def create_plots(data,file_name):
+def create_performance_plots(data,file_name):
   try:
     path=os.path.join("./Imgs/Performance",f"{file_name}")
     
@@ -119,8 +111,12 @@ def create_plots(data,file_name):
     colors=["green","blue","orange","red","black"]
     ax[0].bar(x_labels,y,color=colors,width=0.5)
     
+    for p in ax[0].patches:
+        ax[0].annotate(str(p.get_height()), (p.get_x() * 1.05, p.get_height() * 1.005))
+      
+    ax[0].grid(True)  
     # plot the data on second plot using number of students
-    plot_data=data[["male_students","female_students","absentees"]].map(lambda x: int(x))
+    plot_data=data[["male_students","female_students","absentees"]].map(lambda x: int(x) if x != "*" else None)
     x=["Male","Female","Absentees"]
     y=(plot_data.values[0])
     # plot a pie chart on the first plot that show percentage and numbers
@@ -130,26 +126,35 @@ def create_plots(data,file_name):
     #  save plot to a file
     school_name=data.school_name.values[0].replace("'","")
     year=data.year_of_exam.values[0].replace("'","")
+    
+    fig=add_watermark(fig)
+    
     fig.suptitle(f"UFAULU WA {school_name} MWAKA {year}",fontsize=14)
     plt.savefig(path)
     plt.close(fig)
+    
+    logging.info("Performance Plot created and Saved")
     return path
   
   except Exception as error:
     logging.error(f"{error} occured while creating a plot")
     return None
 
-def get_saved_details(image_file,messenger):
+
+def get_saved_details(image_name,ids_file_path,messenger):
   # get saved details of the image
   try:
-    collection=performance_collection.find_one({"name":image_file})
-    print(collection)
-    return
+    with open(ids_file_path, 'r+') as file:
+      data = json.load(file)
+
+    collection=data["Image data"].get(image_name)
     if collection is not None:
+      
+      logging.info("Getting Saved Details...")
       # check the timediff
       uploaded_at=collection.get("uploaded_at")
       time_difference=datetime.now()-datetime.fromtimestamp(uploaded_at)
-      if time_difference >=29:
+      if time_difference.days >=29:
         image_path=collection.get("file_path")
         # upload_the file again
         image_id=upload_media(image_path,messenger)
@@ -172,24 +177,27 @@ def get_saved_details(image_file,messenger):
     return None
 
 
-def upload_media(image_path,messenger):
-  """
-  Upload media to whatsapp cloud
-  """
+async def save_details(data:dict,id_file_path:str,messenger):
   try:
-    return messenger.upload_media(image_path).get("id")
-  except Exception as error:
-    logging.error(f"{error} while uploading media to cloud")
-    return None
-
-async def save_details(data,messenger):
-  try:
-    image_id=upload_media(data.get("file_path"))
+    
+    def upload_media(image_path,messenger):
+      """
+      Upload media to whatsapp cloud
+      """
+      try:
+        return messenger.upload_media(image_path).get("id")
+      except Exception as error:
+        logging.error(f"{error} while uploading media to cloud")
+        return None
+        
+    image_id=upload_media(data.get("file_path"),messenger)
+    
     if image_id is not None:
       now=datetime.timestamp(datetime.now(pytz.timezone("Africa/Nairobi")))
       data["image_id"]=image_id
       data["uploaded_at"]=now
-      await save_to_mongodb(data)
+      
+      save_to_local(data,id_file_path)
       
       return {"image_id":image_id,
               "message":data.get("message"),
@@ -199,12 +207,179 @@ async def save_details(data,messenger):
     return None
 
 
-async def save_to_mongodb(data):
-  try:
-    collection=performance_collection
-    collection.insert_one(data)
-    return True
-  except Exception as error:
-    logging.error(f"{error} while saving to local")
-    return False
+def save_to_local(new_data, file_path):
+  # Open the file for reading and writing
+  with open(file_path, 'r+') as file:
+    try:
+        # Load the existing JSON data
+        data = json.load(file)
+    except json.JSONDecodeError:
+        # Handle the case where the file is empty or not valid JSON
+        data["Image data"] = {}
+    # Append the new employee data to the 'employees' key
+    data['Image data'][new_data.get("name")]=(new_data)
+
+    # Set the file pointer to the beginning and truncate the file
+    file.seek(0)
+    file.truncate()
+    # Write the updated JSON data back to the file
+    json.dump(data, file, indent=4)
+    logging.info(f"Saved Local data to {file_path}")
+
+
+async def school_comparison(data,messenger):
+  
+  start_year=int(data.get("school_comparison_start_year"))
+  end_year=int(data.get("school_comparison_end_year"))
+  # check the years and swap if necessary
+  if start_year > end_year:
+    temp=start_year
+    start_year=end_year
+    end_year=temp
     
+  exam_type= "csee" if data.get("school_comparison_exam_type") == "1" else "acsee"
+  first_school=data.get("school_comparison_first_school")
+  second_school=data.get("school_comparison_second_school")
+
+  image_name=f"{start_year}_{end_year}_{exam_type}_{first_school.upper()}_{second_school.upper()}.png"
+
+  try:
+    # check if image exists
+    base_path = "./Imgs/Comparison"
+    image_name_variant=[f"{start_year}_{end_year}_{exam_type}_{second_school.upper()}_{first_school.upper()}.png",image_name]
+    
+    for variant_name in image_name_variant:
+      if os.path.exists(os.path.join(base_path,variant_name)):
+        logging.info("Loading saved data")
+        ids_file_path=f'{base_path}/image_ids.json'
+        return get_saved_details(variant_name,ids_file_path,messenger)
+    
+    data = comparison.comparision(start_year, 
+                                end_year, 
+                                exam_type, 
+                                [str(first_school),str(second_school)])
+    
+    if data is not None:
+      file_path=create_comparison_plots(data,image_name)
+      
+      if file_path is not None:
+        data={
+          "file_path":file_path,
+          "name":image_name,
+          "caption": f"Ufaulu kutoka *{start_year} mpaka {end_year}*",
+          "message": f"Mliganisho wa ufaulu wa shule *{first_school}* na *{second_school}*"
+        }
+        id_file_path=f"{base_path}/image_ids.json"
+        response=await save_details(data,id_file_path,messenger)
+        
+        logging.info("Done with school comparison")
+
+        return response
+
+  except Exception as error:
+    logging.error(f"{error} occured in school_comparison")
+    return None
+
+
+
+def create_comparison_plots(data,image_name):
+  
+  image_path=os.path.join("./Imgs/Comparison/",image_name)
+  try:
+    def generate_rows_cols(n):
+      rows = math.ceil(math.sqrt(n))
+      columns = math.ceil(n / rows)
+      logging.info("Done Generating rows and Columns")
+      return rows, columns
+    
+    def generate_plot_axis(ax,rows,colums):
+      logging.info("Generating plot axis from rows and columns")
+      # Yields the plot
+      try:
+        if rows==1 and columns==1:
+          yield ax
+        else:
+          for i in range(rows):    
+            if columns==1:
+              yield ax[i]
+            else:
+              for j in range(colums):
+                yield ax[i,j]
+
+      except StopIteration as e:
+        logging.error("At the end of generation of axises")
+        pass
+
+    rows,columns= generate_rows_cols(len(data))
+    fig,ax=plt.subplots(rows,columns,figsize=((len(data)*4),(len(data)*3+2)))
+    axis=generate_plot_axis(ax,rows,columns)
+    
+    fig.set_facecolor("#eaeaf2")
+
+    for i,year in enumerate(data.keys()):
+      ax=next(axis)
+    
+      # create a dataframe for each year
+      df=pd.DataFrame(data.get(year)).T
+      # convert the data to float
+      df=df.map(lambda x: float(x) if x != "*" else None)
+      # plot the data
+      df.plot(kind="bar",ax=ax,title=year)
+  
+      # Indicate values on the plot
+      for p in ax.patches:
+          ax.annotate(str(p.get_height()), (p.get_x() * 1.0005, p.get_height() * 1.0005))
+      # use custom legend naming
+      ax.legend(["Nafasi Kitaifa","Idadi ya wanafunzi","GPA"])
+      # set the x ticks
+      ax.set_xticklabels(df.index,rotation=0)
+  
+      # set the grid
+      ax.grid(True)
+      # set the face color
+      ax.set_facecolor("#eaeaf2")
+    logging.info("Plots generated successfully")
+  
+    # remove any empty plot
+    if len(data)%2!=0:
+      try:
+        
+        ax=next(axis)
+        ax.axis("off")
+        ax.set_facecolor("#eaeaf2")
+        logging.info("Removed any empty plot")
+        
+      except StopIteration:
+        logging.info("Reached at the end of Generators")
+    
+    fig=add_watermark(fig)
+      
+    # set plot title
+    fig.suptitle("Mlinganisho wa ufaulu",fontsize=14,fontweight="bold")
+    # save the plot
+    plt.savefig(image_path)
+    # # close fig
+    plt.close(fig)
+    
+    logging.info("Created and saved Comparison plot sucessfully")
+
+    return image_path
+    
+  except Exception as error:
+    logging.error(f"{error} while creating comparison plots")
+    return None
+
+
+def add_watermark(fig,image_path="./Imgs/Watermarks/watermark.png"):
+  img = Image.open(image_path)
+  # resize the image
+  img=img.resize((int(img.size[0]/1.5),int(img.size[1]/1.5)))
+  # center the figure
+  fig.figimage(img, 
+               int((fig.bbox.xmax-img.size[0])/2), 
+               int((fig.bbox.ymax-img.size[0])/2), 
+               alpha=0.2, 
+               zorder=1)
+  logging.info("Added Watermark on the Plot")
+
+  return fig
